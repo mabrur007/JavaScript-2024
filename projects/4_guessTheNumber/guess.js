0b4c67560e73c50df6936026ccb39ebee2215a37
mabrur007 <mirmd.mabrur@gmail.com>
2024-01-15 16:10:41 +0600

    p_4 validateGuess done

--- projects/4_guessTheNumber/guess.js
+++ projects/4_guessTheNumber/guess.js
@@ -1,7 +1,7 @@
 const random = parseInt((Math.random() * 100) + 1);
 
 const userInput = document.querySelector("#guessField");
-const submit = document.querySelector("#sut");
+const submit = document.querySelector("#subt");
 const guessSlot = document.querySelector(".guesses");
 const remaining = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
@@ -13,8 +13,32 @@ let prevGuess = [];
 let numberOfGuesses = 1;
 let playGame = true;
 
+if (playGame) {
+    submit.addEventListener('click', function (e) {
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        validateGuess(guess)
+    })
+}
+
 function validateGuess(guess) {
-    //
+    if (isNaN(guess)) {
+        alert('Please enter a valid number');
+    } else if (guess < 1) {
+        alert('Please enter a number more than 1.');
+    } else if (guess > 100) {
+        alert('Please enter a number less than 100');
+    } else {
+        prevGuess.push(guess);
+        if (numberOfGuesses === 11) {
+            displayGuess(guess);
+            displayMessage(`Game over! Random number was: ${random}`);
+            endGame()
+        } else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
 }
 
 function checkGuess(guess) {
@@ -33,4 +57,6 @@ function endGame() {
     //
 }
 
-function startGame
+function startGame() {
+    //
+}
