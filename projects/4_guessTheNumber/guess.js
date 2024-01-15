7782c83651712dc18c7158eb7d2f03e9813efc00
mabrur007 <mirmd.mabrur@gmail.com>
2024-01-15 15:39:02 +0600

    p_4 pre functional work

--- projects/4_guessTheNumber/guess.js
+++ projects/4_guessTheNumber/guess.js
@@ -6,3 +6,31 @@ const guessSlot = document.querySelector(".guesses");
 const remaining = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
 const startOver = document.querySelector(".resultParas");
+
+// essential parameters for the game
+let p = document.createElement("p");
+let prevGuess = [];
+let numberOfGuesses = 1;
+let playGame = true;
+
+function validateGuess(guess) {
+    //
+}
+
+function checkGuess(guess) {
+    //
+}
+
+function displayGuess(guess) {
+    //
+}
+
+function displayMessage(message) {
+    //
+}
+
+function endGame() {
+    //
+}
+
+function startGame
